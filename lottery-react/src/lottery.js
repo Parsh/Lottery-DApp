14b116abdda34a60811f7955c5d66d4536a4d2f7
parsh24 <parsh.cosmos11@gmail.com>
2018-08-19 14:18:33 +0530

    Lottery Contract: JS Instance Created

---
+++ lottery-react/src/lottery.js
@@ -0,0 +1,63 @@
+import web3 from './web3';
+
+const address = '0x68AfDe447a6e5c5925d451663DC3d8c4a262C8Cf';
+const abi = [{
+    "constant": true,
+    "inputs": [],
+    "name": "manager",
+    "outputs": [{
+        "name": "",
+        "type": "address"
+    }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [],
+    "name": "pickWinner",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [],
+    "name": "getPlayers",
+    "outputs": [{
+        "name": "",
+        "type": "address[]"
+    }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [],
+    "name": "enter",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [{
+        "name": "",
+        "type": "uint256"
+    }],
+    "name": "players",
+    "outputs": [{
+        "name": "",
+        "type": "address"
+    }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+}];
+
+export default new web3.eth.Contract(abi, address);
